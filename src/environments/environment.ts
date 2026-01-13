40d7bb793cd96bd82110a93808c0e4ff92239452
Irving Jared Escalante Sianuqui <irvingsianuqui@gmail.com>
2026-01-12 19:54:51 -0700

    Actualizacion de env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api',
-  //apiUrl: 'https://backend-coursehub.onrender.com/api',
-  //apiUrlForStatics:'https://backend-coursehub.onrender.com',
-  apiUrlForStatics:'http://localhost:8000'
+  //apiUrl: 'http://localhost:8000/api',
+  apiUrl: 'https://backend-coursehub.onrender.com/api',
+  apiUrlForStatics:'https://backend-coursehub.onrender.com',
+  //apiUrlForStatics:'http://localhost:8000'
 };
